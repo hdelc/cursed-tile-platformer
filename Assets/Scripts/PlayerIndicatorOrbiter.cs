5347f920bf8da09ffb3d66de613b02c1223c27b2
hdelc02 <hdelc02@gmail.com>
2021-11-04 21:33:53 -0400

    Implement indicator

--- Assets/Scripts/PlayerIndicatorOrbiter.cs
+++ Assets/Scripts/PlayerIndicatorOrbiter.cs
@@ -19,14 +19,20 @@ class PlayerIndicatorOrbiter : MonoBehaviour
   {
     GameObject targetCollectible = GameObject.Find("Collectible");
     Vector2 targetRay = targetCollectible.transform.position - parentPlayer.transform.position;
+    Color rayColor;
     if (targetRay.magnitude > vanishProximity)
     {
+      rayColor = Color.green;
       spriteRenderer.enabled = true;
-      transform.position = orbitRadius * targetRay.normalized;
+      transform.position = (orbitRadius * targetRay.normalized) + (Vector2)parentPlayer.transform.position;
       transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, targetRay)));
     }
     else
+    {
+      rayColor = Color.red;
       spriteRenderer.enabled = false;
+    }
+    Debug.DrawRay(parentPlayer.transform.position, targetRay, rayColor, Time.fixedDeltaTime);
   }
 }
 
